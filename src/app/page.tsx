b16fbab041b4f601a82be710cf9b2b040b8566b9
Anmol Agarwal <creativeanmolblogs@gmail.com>
2026-02-23 13:22:05 +0530

    Change redirect to use useEffect in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from 'next/navigation';
+'use client'
 
-export default function RootPage() {
-  redirect('/dashboard/chat');
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/dashboard/chat')
+  }, [])
+
+  return <div>Loading...</div>
 }
